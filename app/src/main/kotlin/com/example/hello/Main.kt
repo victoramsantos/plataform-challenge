87244e33be8fff7906a2480e152bea04965b56a3
Victor Amorim <victor.amsantos@gmail.com>
2020-03-15 13:46:21 -0300

    Adding hpa

--- app/src/main/kotlin/com/example/hello/Main.kt
+++ app/src/main/kotlin/com/example/hello/Main.kt
@@ -6,15 +6,31 @@ import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import java.util.logging.Logger
+import kotlin.math.sqrt
 
 fun main(args: Array<String>) {
     val target = System.getenv("TARGET") ?: "World"
     val port = System.getenv("PORT") ?: "8080"
     embeddedServer(Netty, port.toInt()) {
         routing {
-            get("/") {
+            get("/{stress?}") {
+                println("Handling request")
+                val stress = call.parameters["stress"]?:""
+                if(stress.isNotEmpty()){
+                    stressing()
+                }
                 call.respondText("Hello $target!", ContentType.Text.Html)
             }
         }
     }.start(wait = true)
+}
+
+fun stressing() : Double{
+    println("Stressing request")
+    var x : Double = 0.0
+    for(i in 0..10000000){
+        x += sqrt(i.toDouble())
+    }
+    return x
 }
